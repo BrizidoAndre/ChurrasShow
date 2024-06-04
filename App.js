882b67ce1bd6fb18e88f3b40958c302665599d25
EvertonAraujo88 <evertonaraujosenai@gmail.com>
2024-06-04 10:01:30 -0300

    incluinfo fonte lato

--- App.js
+++ App.js
@@ -3,24 +3,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import {  Button, StyleSheet, Text, View } from 'react-native';
 
-import { useFonts } from 'expo-font';
-<<<<<<< HEAD
-import Login from './pages/login/login';
-import Home from './pages/home/home';
-
-import Main from './pages/main/main';
-=======
 import Main from './src/pages/main/main';
 import Home from './src/pages/home/home';
 import Login from './src/pages/login/login';
->>>>>>> develop
+
+import { useFonts, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({
-    "Cintra": require("./assets/fonts/CintraRegular.ttf")
+    "Cintra": require("./assets/fonts/CintraRegular.ttf"),
+    "lato_regular": Lato_400Regular,
+    "lato_bold": Lato_700Bold
   });
 
 
--- src/components/button/style.js
+++ src/components/button/style.js
@@ -1,12 +1,21 @@
 import styled from "styled-components";
+import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
 
 export const ButtonLogin = styled.TouchableOpacity`
     width: 90%;
     height:44px;
-
+    border: #CAA858;
     border-radius: 20px;
+    background-color: #000000;
     
     align-items: center;
     justify-content: center;
-    border: #CAA858;
+    
+`
+export const TextButton = styled.Text`
+    font-family:'lato_regular';
+
+    font-size:24px;
+    color: black
+
 `
