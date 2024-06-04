b29badbb67d448c538f6a79e26d009289105a3c0
Gabriel <gbielmarch@gmail.com>
2024-06-04 10:41:39 -0300

    Card Feito

---
+++ src/components/Card/Card.js
@@ -0,0 +1,169 @@
+import { Image, SectionList, Text, View } from "react-native"
+import { CardContainer, BoxImage, BoxText, TitleList, BoxCard, PriceText, BoxTextPrice,FoodImage } from "./Style"
+
+export const CardPacoteBasico = () => {
+    const PacoteBasico = [
+        {
+            title: 'PACOTE BÁSICO',
+            data: ['100 convidados', 'Duração: 4h']
+        }
+    ]
+
+    return (
+        <CardContainer>
+            <BoxCard>
+                <BoxImage>
+                    <FoodImage source={require('../../assets/image 1.png')} />
+                </BoxImage>
+
+                <BoxText>
+                    <SectionList
+                        sections={PacoteBasico}
+                        keyExtractor={(item, index) => item + index}
+
+                        renderItem={({ item }) => (
+                            <View>
+                                <Text> - {item}</Text>
+                            </View>
+                        )}
+                        renderSectionHeader={({ section: { title } }) => (
+                            <TitleList>{title}</TitleList>
+                        )}
+                    />
+
+                    <PriceText>R$0000,00</PriceText>
+
+                </BoxText>
+            </BoxCard>
+
+        </CardContainer>
+    )
+}
+
+export const CardPacotePremium = () => {
+
+    const PacotePremium = [
+        {
+            title: 'PACOTE PREMIUM',
+            data: ['150 convidados', 'Duração: 6h']
+        }
+    ]
+
+    return (
+        <CardContainer>
+            <BoxCard>
+                <BoxImage>
+                    <FoodImage source={require('../../assets/image 2.png')} />
+                </BoxImage>
+
+                <BoxText>
+
+                    <SectionList
+                        sections={PacotePremium}
+                        keyExtractor={(item, index) => item + index}
+
+                        renderItem={({ item }) => (
+                            <View>
+                                <Text> - {item}</Text>
+                            </View>
+                        )}
+                        renderSectionHeader={({ section: { title } }) => (
+                            <TitleList>{title}</TitleList>
+                        )}
+                    />
+
+                    <PriceText>R$0000,00</PriceText>
+                </BoxText>
+            </BoxCard>
+
+        </CardContainer>
+    )
+}
+
+export const CardPacoteBasicoPendente = () => {
+    const PacoteBasico = [
+        {
+            title: 'PACOTE BÁSICO',
+            data: ['100 convidados', 'Duração: 4h']
+        }
+    ]
+
+    return (
+        <CardContainer>
+            <BoxCard>
+                <BoxImage>
+                    <FoodImage source={require('../../assets/image 1.png')} />
+                </BoxImage>
+
+                <BoxText>
+                    <SectionList
+                        sections={PacoteBasico}
+                        keyExtractor={(item, index) => item + index}
+
+                        renderItem={({ item }) => (
+                            <View>
+                                <Text> - {item}</Text>
+                            </View>
+                        )}
+                        renderSectionHeader={({ section: { title } }) => (
+                            <TitleList>{title}</TitleList>
+                        )}
+                    />
+                    <BoxTextPrice>
+                        <PriceText>R$0000,00</PriceText>
+
+                        <Image source={require('../../assets/PerfilCard.png')} />
+
+                    </BoxTextPrice>
+
+                </BoxText>
+            </BoxCard>
+
+        </CardContainer>
+    )
+}
+
+export const CardPacotePremiumPendente = () => {
+
+    const PacotePremium = [
+        {
+            title: 'PACOTE PREMIUM',
+            data: ['150 convidados', 'Duração: 6h']
+        }
+    ]
+
+    return (
+        <CardContainer>
+            <BoxCard>
+                <BoxImage>
+                    <FoodImage source={require('../../assets/image 2.png')} />
+                </BoxImage>
+
+                <BoxText>
+
+                    <SectionList
+                        sections={PacotePremium}
+                        keyExtractor={(item, index) => item + index}
+
+                        renderItem={({ item }) => (
+                            <View>
+                                <Text> - {item}</Text>
+                            </View>
+                        )}
+                        renderSectionHeader={({ section: { title } }) => (
+                            <TitleList>{title}</TitleList>
+                        )}
+                    />
+
+                    <BoxTextPrice>
+                        <PriceText>R$0000,00</PriceText>
+
+                        <Image source={require('../../assets/PerfilCard.png')} />
+
+                    </BoxTextPrice>
+                </BoxText>
+            </BoxCard>
+
+        </CardContainer>
+    )
+}
---
+++ src/components/Card/Style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.View`
+    width: 90%;
+    height: 109px;
+    background-color: #F2E6D0;
+    margin-bottom: 10px;
+    border-radius: 10px;
+`
+export const BoxCard = styled.View`
+    width: 90%;
+    margin-top: 7px;
+    justify-content: space-evenly;
+    gap: 15px;
+    align-items: center;
+    flex-direction: row;
+`
+
+export const TitleList = styled.Text`
+    font-weight: bold;
+    font-size: 15px;
+`
+
+export const BoxImage = styled.View`
+   border-radius: 10px;
+    width: auto;
+`
+
+export const FoodImage = styled.Image`
+    border-radius: 10px;
+`
+
+
+export const BoxText = styled.View`
+    width: auto;
+`
+
+export const PriceText = styled.Text`
+    font-weight: bold;
+    font-size: 15px;
+`
+
+export const BoxTextPrice = styled.View`
+    flex-direction: row;
+    justify-content: space-evenly;
+    gap: 30px;
+`
